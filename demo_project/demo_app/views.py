09b0492462d79ce8de0b9a9a003b801100294ee3
k-souvatzidaki <kwn.souv@gmail.com>
2023-02-11 17:08:55 +0200

    requirements.txt

--- demo_project/demo_app/views.py
+++ demo_project/demo_app/views.py
@@ -1,6 +1,8 @@
+from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
+@require_http_methods(['GET'])
 def index(request):
     return HttpResponse('Hello world!')
